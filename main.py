0d808f8f808ed20ee715e54b0d1d24e9d5c70c21
Youssef Mohamed <uif721@gmail.com>
2023-10-27 00:47:29 +0200

    Simple Youtube to mp3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 video = YouTube(str(input("Enter the Video's URL : ")))
 print("Processing...\n")
 mp3 = video.streams.filter(only_audio=True).first()
-print("Enter Destination or leave blank for currect directory \n")
+print("Enter Destination or leave blank for current directory \n")
 destination =str(input(">> ")) or '.'
 print("Downloading...\n")
 output = mp3.download(output_path=destination)
